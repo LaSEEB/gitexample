9eb3d39a4245a7f1a720d33ebfd8bbb754588592
Nuno Fachada <faken@fakenmc.com>
2016-03-11 10:48:23 +0000

    More efficient function

--- larger_than_zero.m
+++ larger_than_zero.m
@@ -2,10 +2,4 @@ function y = larger_than_zero(x)
 % LARGER_THAN_ZERO How many elements
 % of x are larger than zero?
 
-y = 0;
-n = numel(x);
-for i = 1:n
-    if x(i) > 0
-        y = y + 1;
-    end;
-end;
+y = sum(x > 0);
